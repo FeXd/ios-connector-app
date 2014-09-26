a2b7a3fdf7b953d6ec00771880710fb0d76ba6cd
Guillermo Kardolus <kardolus@gmail.com>
2014-09-25 19:08:18 -0700

    Testing git

--- mHealth/Connector.swift
+++ mHealth/Connector.swift
@@ -2,7 +2,7 @@
 //  Connector.swift
 //  mHealth
 //
-//  Created by Mister Clipping on 9/25/14.
+//  Created by Guillermo Kardolus on 9/25/14.
 //  Copyright (c) 2014 Open mHealth. All rights reserved.
 //
 
