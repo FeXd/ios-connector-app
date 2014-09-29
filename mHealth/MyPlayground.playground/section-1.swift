80bc8b9064da4927292b641e103e8f3ab118a912
Guillermo Kardolus <kardolus@gmail.com>
2014-09-29 08:47:23 -0400

    Added External Modules SwiftyJSON and AlamoFire

--- mHealth/MyPlayground.playground/section-1.swift
+++ mHealth/MyPlayground.playground/section-1.swift
@@ -8,8 +8,13 @@ import HealthKit
 // read up on the HealthKit Framework:
 // https://developer.apple.com/library/ios/documentation/HealthKit/Reference/HealthKit_Framework/index.html#//apple_ref/doc/uid/TP40014707
 
-var meters = HKUnit.meterUnit()
+let meters = HKUnit.meterUnit()
 
+// HKQuantity stores the value for a given unit
+// I'm guessing you use HKUnit to set the unit of the HKQuantity
+// Then convert the HKQuantity with other HKUnit refs/methods
+// Legal work required for HealthKit App... T&C
 
-
+// Remarks from FrameWork Reference:
+// - If the user consents, you may share his or her HealthKit data with a third party for medical research.
 
