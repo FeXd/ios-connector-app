c813a01f7b938eb2e729048ff7523382c0470d4d
Guillermo Kardolus <kardolus@gmail.com>
2014-10-04 15:42:37 -0400

    added a healthkit unit conversion example

--- mHealth/MyPlayground.playground/section-1.swift
+++ mHealth/MyPlayground.playground/section-1.swift
@@ -8,13 +8,17 @@ import HealthKit
 // read up on the HealthKit Framework:
 // https://developer.apple.com/library/ios/documentation/HealthKit/Reference/HealthKit_Framework/index.html#//apple_ref/doc/uid/TP40014707
 
-let meters = HKUnit.meterUnit()
+println("Now: \(NSDate())")
 
-// HKQuantity stores the value for a given unit
-// I'm guessing you use HKUnit to set the unit of the HKQuantity
-// Then convert the HKQuantity with other HKUnit refs/methods
-// Legal work required for HealthKit App... T&C
+// Testing Unit Conversion
+let weightType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+var massNumber = 40.0
+let weightValue = HKQuantity(unit: HKUnit(fromString: "kg"), doubleValue: massNumber)
+let metadata = [ HKMetadataKeyWasUserEntered : true ]
+let sample = HKQuantitySample(type: weightType, quantity: weightValue,
+    startDate: NSDate(), endDate: NSDate(), metadata: metadata)
 
-// Remarks from FrameWork Reference:
-// - If the user consents, you may share his or her HealthKit data with a third party for medical research.
+println("Weight in KG: \(weightValue)")
+println("Weight in Pound: \(weightValue.doubleValueForUnit(HKUnit.poundUnit()))")
+println("Weight in Grams: \(weightValue.doubleValueForUnit(HKUnit.gramUnit()))")
 
